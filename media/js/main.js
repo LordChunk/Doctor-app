b762ef8e2a74cac20304989ad99feee964ba7667
LordChunk <LordChunk@users.noreply.github.com>
2017-12-04 13:48:18 +0100

    Fixed nav bar and changed the colour theme.

--- media/js/main.js
+++ media/js/main.js
@@ -70,12 +70,16 @@ window.onpopstate = function() {
 $(document).ready(function () {
     //Set the side nav right above the screen on load
     //Somehow this doesn't always work, not quite sure why
-    $("#side-nav").css("top", $('#landing').height() - $('#side-nav').height()); //This loads the nav underneath the landing page right above the nav bar
+    var $navHeight =  $('#side-nav').height();
+    $("#side-nav").css("top", $('header').height() - $('nav').height()); //This loads the nav underneath the landing page right above the nav bar
 
     //Navbar animation
     //Navbar now also closes when clicking any other item withing the nav bar
     $('body').on('click', '#close_mobile_nav, #side-nav *', function (){
-        $('#side-nav').animate({"top": - $('#side-nav').height()}, "slow"); //This moves the nav bar above the nav bar + landing page height
+        $('#side-nav').animate({"height": 0}, "slow", function(){
+            $('#side-nav').css("overflow", "hidden");
+        }); 
+        //This moves the nav bar above the nav bar + landing page height
     });
 
     //Do redirect for desktop and open nav bar for mobile
@@ -83,7 +87,9 @@ $(document).ready(function () {
     {
         //Change attribute href and check for click event
         $('body').on('click', '#open_mobile_nav', function (){
-            $('#side-nav').animate({"top": 0}, "slow");
+            $('#side-nav').animate({"height": $navHeight}, "slow",function(){
+                $('#side-nav').css("overflow", "");
+            });
         });
     }
     else
